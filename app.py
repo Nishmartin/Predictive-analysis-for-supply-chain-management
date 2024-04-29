cf8f06e4886afc79a29935ae26030eb584b2822e
Nishmartin <168201875+Nishmartin@users.noreply.github.com>
2024-04-30 05:28:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,27 +15,30 @@ from DecisionTree import best_model  # Assuming your best model is defined as 'b
 file_path = os.path.join('cleaned_dataset_updated.csv')
 df = pd.read_csv(file_path)
 
-
-# Define features and target variable
-features = ['Sales', 'Order Item Quantity', 'Benefit per order']
-target_variable = 'Order Profit Per Order'
-
 # Set title
 st.title("Supply Chain Predictive Analytics")
-
 # Add a sidebar
 st.sidebar.title("Input Parameters")
 
-# Add input fields for features
-sales = st.sidebar.number_input("Sales", value=0.0)
-quantity = st.sidebar.number_input("Order Item Quantity", value=0)
-benefit = st.sidebar.number_input("Benefit per order", value=0.0)
+# Select Target Variable
+target_variable = st.sidebar.selectbox("Select Target Variable", df.columns)
+
+# Select Features
+selected_features = st.sidebar.multiselect("Select Features", df.columns)
+
+# Input Values for Features
+feature_values = {}
+for feature in selected_features:
+    if df[feature].dtype == 'object':
+        feature_values[feature] = st.sidebar.selectbox(f"Select {feature}", df[feature].unique())
+    else:
+        feature_values[feature] = st.sidebar.number_input(f"Enter {feature}", value=0.0)
 
 # Create a button to make predictions
 if st.sidebar.button("Predict"):
     # Create a DataFrame with user inputs
-    user_input = pd.DataFrame({"Sales": [sales], "Order Item Quantity": [quantity], "Benefit per order": [benefit]})
+    user_input_df = pd.DataFrame(feature_values, index=[0])
     # Make prediction
-    prediction = best_model.predict(user_input)
+    prediction = best_model.predict(user_input_df)
     # Display prediction
-    st.write(f"Predicted Order Profit Per Order: {prediction[0]}")
+    st.write(f"Predicted {target_variable}: {prediction[0]}")
